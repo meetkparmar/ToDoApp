f355f4eba1d73ce7a5ea0e84121a4ef5ebbe736c
prudhvi reddy <prudhvireddy.m01@gmail.com>
2020-04-18 14:49:26 +0530

    inapp updates init

--- app/src/main/java/com/prudhvir3ddy/todo_app_gettingthingsdone/view/splash/SplashActivity.kt
+++ app/src/main/java/com/prudhvir3ddy/todo_app_gettingthingsdone/view/splash/SplashActivity.kt
@@ -1,10 +1,17 @@
 package com.prudhvir3ddy.todo_app_gettingthingsdone.view.splash
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.play.core.appupdate.AppUpdateInfo
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.model.ActivityResult
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
 import com.google.firebase.iid.FirebaseInstanceId
 import com.prudhvir3ddy.todo_app_gettingthingsdone.R.layout
 import com.prudhvir3ddy.todo_app_gettingthingsdone.storage.SharedPrefs
@@ -15,7 +22,12 @@ import org.koin.android.ext.android.inject
 
 class SplashActivity : AppCompatActivity() {
 
-  val TAG = SplashActivity::class.simpleName
+  companion object {
+    const val MY_REQUEST_CODE = 1
+    val TAG = SplashActivity::class.simpleName
+  }
+
+  private lateinit var appUpdateManager: AppUpdateManager
 
   private val sharedPrefs: SharedPrefs by inject()
 
@@ -24,6 +36,36 @@ class SplashActivity : AppCompatActivity() {
     setContentView(layout.activity_splash)
 
     getFcmToken()
+
+    handleAppFlow()
+
+    handleAppUpdateManager()
+
+  }
+
+  private fun handleAppUpdateManager() {
+    appUpdateManager = AppUpdateManagerFactory.create(this)
+    val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+    appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+      if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+        // For a flexible update, use AppUpdateType.FLEXIBLE
+        && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+      ) {
+        requestUpdate(appUpdateInfo)
+      }
+    }
+  }
+
+  private fun requestUpdate(appUpdateInfo: AppUpdateInfo) {
+    appUpdateManager.startUpdateFlowForResult(
+      appUpdateInfo,
+      AppUpdateType.FLEXIBLE, //  HERE specify the type of update flow you want
+      this,   //  the instance of an activity
+      MY_REQUEST_CODE
+    )
+  }
+
+  private fun handleAppFlow() {
     when {
       sharedPrefs.getLogin() -> {
         startActivity(Intent(this, TasksActivity::class.java))
@@ -50,6 +92,21 @@ class SplashActivity : AppCompatActivity() {
         // Log and toast
         Log.d(TAG, token)
       })
-
   }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+    if (requestCode == MY_REQUEST_CODE) {
+      when (resultCode) {
+        Activity.RESULT_OK -> {
+        }
+        Activity.RESULT_CANCELED -> {
+        }
+        ActivityResult.RESULT_IN_APP_UPDATE_FAILED -> {
+        }
+      }
+    }
+  }
+
+
 }
